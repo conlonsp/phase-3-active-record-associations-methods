4003814de37796ffed4b63ebf0b891e02cb08b07
samuelconlon <conlonsp@yahoo.com>
2023-01-23 20:37:49 -0500

    commit tree 7ec85b937d80657ed41ad267f9aa8f289119dbf4

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,15 +2,49 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    song = songs.first
+    song.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    genres.count
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # has_many :songs
+  # has_many :genres, through: :songs
+
+    # genres.first
+
+    # songs.count
+
+    # genres.count
